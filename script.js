2f8eea5f812dac049dc70bf4120239a3e7957f9a
Your Name <you@example.com>
2022-09-08 11:44:05 -0500

    first commit

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ itemsliderbar.forEach(function(menu,index){
         menu.classList.toggle("block")
     })
 })
+
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smallImg = document.querySelectorAll(".product-content-left-small-img img")
+smallImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click",function(){
+        bigImg.src= imgItem.src
+    })
+})
